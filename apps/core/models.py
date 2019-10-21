7cf04c6b274cd178740b8e358608ab02cb5f9fc8
Diego Costa <diegocostacmp@gmail.com>
2019-10-21 10:47:15 -0300

    Adding apps avaliacao, comentario and apis by app

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,10 +1,15 @@
 from django.db import models
-
+from apps.atracao.models import Atracao
+from apps.comentario.models import Comentario
+from apps.avaliacao.models import Avaliacao
 # Create your models here.
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
-
+    atracao = models.ManyToManyField(Atracao)
+    comentario = models.ManyToManyField(Comentario)
+    avaliacao = models.ManyToManyField(Avaliacao)
+    
     def __str__(self):
         return self.nome
--- turismo/settings.py
+++ turismo/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'apps.core',
+    'apps.atracao',
+    'apps.comentario',
+    'apps.avaliacao',
 ]
 
 MIDDLEWARE = [
