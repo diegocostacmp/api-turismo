8e2abcab424385fc703da8481ebc72a5297669ef
Diego Costa <diegocostacmp@gmail.com>
2019-10-24 10:11:05 -0300

    Adding field observation

--- apps/atracao/models.py
+++ apps/atracao/models.py
@@ -6,7 +6,7 @@ class Atracao(models.Model):
     horario_func = models.TextField()
     idade_minima = models.IntegerField()
     foto = models.ImageField(upload_to='atracao', blank=True, null=True)
-    
+    observacao = models.CharField(max_length=150, blank=True, null=True)
 
     def __str__(self):
         return self.nome
